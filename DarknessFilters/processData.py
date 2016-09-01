5b38ce90b2d3fd3fb42df6ebdb1a94f7366412c7
mstrader <mstrader@physics.ucsb.edu>
2016-08-31 17:56:40 -0700

    fixed small bugs and cleaned up processData.py

--- DarknessFilters/processData.py
+++ DarknessFilters/processData.py
@@ -2,10 +2,11 @@ import numpy as np
 import makeTemplate as mT
 import makeFilters as mF
 import os
+import sys
 
-def processData(directory,filterType='matched'):
+def processData(directory,filterType='matched',isVerbose=False):
     '''
-    Loop through .dat files in the directory and create filter coefficients and thresholds for each
+    Loop through .dat files in the directory and create filter coefficient files for each
     INPUTS:
     directory - path for the folder containing the data
     filterType - type of filter to export 
@@ -16,13 +17,14 @@ def processData(directory,filterType='matched'):
         if os.path.isfile(os.path.join(directory,item)) and item.endswith('.dat'):
             fileList.extend(item)
     
-    
-    for fileName in fileList
+    #loop through all the phase stream data files in the directories
+    for index, fileName in enumerate(fileList):
+
         #load data
         rawData=np.loadtxt(os.path.join(directory,fileName))
 
         #make template
-        template,time , noiseSpectrumDict, _, _ = mT.makeTemplate \       
+        template, time , noiseSpectrumDict, _, _ = mT.makeTemplate \       
         (rawData,nSigmaTrig=5.,numOffsCorrIters=3,isVerbose=isVerbose,isPlot=isPlot, sigPass=.5)
     
         #make filter
@@ -36,5 +38,18 @@ def processData(directory,filterType='matched'):
             filterCoef=mF.makeSuperMatchedFilter(template, noiseSpectrumDict['noiseSpectrum'], coef[1], nTaps=50, tempOffs=75)
         else:
             raise ValueError('proccessData: filterType not defined')
-        #save filter coefficients to an appropriately named file    
+        
+        #save filter coefficients to an appropriately named file
+        saveFileName, _= os.path.splitext(fileName)
+        saveFileName+='_filter.dat'
+        np.savetxt(saveFileName, filterCoef)
+        
+        #print progress to terminals
+        if isVerbose:
+            if index!=len(fileList)-1
+                perc=round(float(index)/(len(fileList)-1))
+                sys.stdout.write("Percent of filters created: %.1f%%  \r" % (perc) )
+                sys.stdout.flush()
+            else:
+                print "processData: Percent of filters created: 100%"  
         
