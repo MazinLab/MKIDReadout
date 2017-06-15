f1a0446b4408807e24374641ff452c86159d4b72
neelay <neelay@physics.ucsb.edu>
2017-06-14 18:37:06 -0700

    Integrated new WS peakfinding w/ WideAna

--- DataReadout/Setup/WideSweep/WSFitML/WSFitFilt.py
+++ DataReadout/Setup/WideSweep/WSFitML/WSFitFilt.py
@@ -18,7 +18,7 @@ class WSTemplateFilt:
         filtMagsDB = self.trainData.filterMags(self.trainData.magsdb)
         self.template = np.zeros(self.winSize)
         for loc in self.trainData.peakLocs:
-            self.template += filtMagsDB[loc-self.winSize/2:loc+self.winSize/2]
+            self.template += filtMagsDB[int(loc-self.winSize/2):int(loc+self.winSize/2)]
 
         self.template /= len(self.trainData.peakLocs)
         plt.plot(self.template)
@@ -29,18 +29,22 @@ class WSTemplateFilt:
             raise Exception('Train template first!')
         np.savetxt(filename, self.template)
 
+    def loadTemplate(self, filename):
+        self.template = np.loadtxt(filename)
+
     def inferPeaks(self, inferenceFile, sigThresh=0.75, nDerivChecks=7, nDerivSlack=0):
         if self.template is None:
             raise Exception('Train or load template first!')
 
         self.inferenceData = WSFitMLData([inferenceFile])
+        self.inferenceFile = inferenceFile
         filtMagsDB = self.inferenceData.filterMags(self.inferenceData.magsdb)
         tempFiltMagsDB = np.correlate(filtMagsDB, self.template, mode='same')
 
         threshold = sigThresh*np.std(tempFiltMagsDB)
         triggerBooleans = tempFiltMagsDB[nDerivChecks:-nDerivChecks-1] > threshold
         print 'threshold', threshold
-        plt.plot(tempFiltMagsDB[2010:4010])
+        #plt.plot(tempFiltMagsDB[2010:4010])
 
         derivative = np.diff(tempFiltMagsDB)
         negDeriv = derivative <= 0
@@ -55,18 +59,42 @@ class WSTemplateFilt:
         peakCondition1 = posDerivChecksSum >= (nDerivChecks - nDerivSlack)
         peakCondition01 = np.logical_and(peakCondition0,peakCondition1)
         peakBooleans = np.logical_and(triggerBooleans,peakCondition01)
-
         self.peakIndices = np.where(peakBooleans)[0] + nDerivChecks
-        plt.plot(triggerBooleans[2000:4000]*tempFiltMagsDB[2010:4010])
-        plt.show()
-        saveFile = inferenceFile.split('.')[0]
-        saveFile += '-ml.txt'
-        np.savetxt(saveFile, self.peakIndices)
+        #plt.plot(triggerBooleans[2000:4000]*tempFiltMagsDB[2010:4010])
+        #plt.show()
+
+    def markCollisions(self, resBWkHz=500):
+        if self.peakIndices is None:
+            raise Exception('Infer peak locations first!')
+        minResSpacing = resBWkHz/self.spacing #resonators must be separated by this number of points
+        peakSpacing = np.diff(self.peakIndices)
+        collisionMask = peakSpacing<minResSpacing
+        collisionInds = np.where(collisionMask)[0] #locations in peakIndices where there are collisions
+        goodPeakInds = np.where(np.logical_not(collisionMask))[0]
+        self.badPeakIndices = self.peakIndices[collisionInds]
+        self.goodPeakIndices = self.peakIndices[goodPeakInds]
+
+    def saveInferenceFile(self):
+        goodSaveFile = self.inferenceFile.split('.')[0]
+        badSaveFile = self.inferenceFile.split('.')[0]
+        goodSaveFile += '-ml-good.txt'
+        badSaveFile += '-ml-bad.txt'
+
+        np.savetxt(goodSaveFile, self.goodPeakIndices)
+        np.savetxt(badSaveFile, self.badPeakIndices)
 
 if __name__=='__main__':
     mdd = os.environ['MKID_DATA_DIR']
-
+    templateDir = '.'
+    if len(sys.argv)<3:
+        raise Exception('Need to specify WS and template files')
+    
+    wsFile = os.path.join(mdd, sys.argv[1])
+    templateFile = os.path.join(templateDir, sys.argv[2])
+    
     wsFilt = WSTemplateFilt()
-    wsFilt.makeTemplate([os.path.join(mdd, 'Hexis_WS_FL3.txt')])
-    wsFilt.inferPeaks(os.path.join(mdd, 'Hojira_FL3.txt'))
+    wsFilt.loadTemplate(templateFile)
+    wsFilt.inferPeaks(wsFile)
+    wsFilt.markCollisions()
+    wsFilt.saveInferenceFile()
 
---
+++ DataReadout/Setup/WideSweep/WSFitML/makeWSFitTemplate.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os, sys
+from WSFitFilt import WSTemplateFilt
+
+templateDir = '.'
+templateFn = 'Hexis_FL3-template.txt'
+rawWSFnList = ['Hexis_WS_FL3.txt']
+
+mdd = os.environ['MKID_DATA_DIR']
+for i,fn in enumerate(rawWSFnList):
+    rawWSFnList[i] = os.path.join(mdd, fn)
+
+wsFit = WSTemplateFilt()
+wsFit.makeTemplate(rawWSFnList)
+wsFit.saveTemplate(os.path.join(templateDir, templateFn))
