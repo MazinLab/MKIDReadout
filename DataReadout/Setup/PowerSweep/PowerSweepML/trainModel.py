6b3feaacaaddf7b4184c3439d06aa2717c27f666
Neelay <neelay@physics.ucsb.edu>
2018-04-12 11:35:23 -0700

    Removed old ML code and consolidated PowerSweep directory

--- DataReadout/Setup/PowerSweep/PowerSweepML/trainModel.py
+++ DataReadout/Setup/PowerSweep/PowerSweepML/trainModel.py
@@ -1,3 +1,13 @@
+'''
+Script to train ML model. Model architecture
+is defined in PSFitNNModel, and parameters
+and training data are defined in specified 
+config file.
+Usage: python trainNNModel.py <mlConfigFile>
+Trained model is saved in .meta file in 
+modelDir specified in config file.
+'''
+
 from PSFitNNModel import *
 from readDict import readDict
 
