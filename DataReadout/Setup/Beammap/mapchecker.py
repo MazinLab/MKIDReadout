9105c477ced87b9eb4c85dec0f946390663a7b76
Noah Swimmer <nswimmer@physics.ucsb.edu>
2018-07-12 12:27:00 -0700

    Commented necessary changes to make, removed unnecessary functions that were incorporated into the feedline class

--- DataReadout/Setup/Beammap/mapchecker.py
+++ DataReadout/Setup/Beammap/mapchecker.py
@@ -12,23 +12,17 @@
 import numpy as np
 import glob
 import matplotlib.pyplot as plt
-import feedline
+from MKIDDigitalReadout.DataReadout.Setup.Beammap import feedline
 
 design_feedline=np.loadtxt("mec_feedline.txt")
-
 feedline_file_name="finalMap_20180605"
-
-#Beammap is read in where each line is a pixel and the columns, left to right, are
-#ResID, flag, x-coordinate, y-coordinate
 beammap=np.loadtxt(r"beammapTestData/test/finalMap_20180605.txt")
-
-resIDs_and_freqs=glob.glob(r"beammapTestData/test/ps_*")
-
-FreqSweepFiles = glob.glob("beammapTestData/test/ps_*")
+freqsweeps = glob.glob("beammapTestData/test/ps_*")
 
 
-#Define a function to see which flags appear most (or least), mostly just a way to
-#Quanitfy the 'goodness' of an array
+# Define a function to see which flags appear most (or least), mostly just a way to
+# Quanitfy the 'goodness' of an array
+# This should be either added into the feedline class or updated to account for the new data structure
 def flag_counter (beammap_array) :
     flag_list=[0,0,0,0,0,0,0]
     for  n in range(len(beammap_array)):
@@ -48,9 +42,11 @@ def flag_counter (beammap_array) :
             flag_list[6]=flag_list[6]+1
     return flag_list
 
-#Create an array map where each element is the design freqency at a given pixel coordinate
-design_array=np.ndarray((146,140))
+'''
+Come back to this function later if necessary, used during development to analyze a full array (as opposed to only feedlines)
 
+Create an array map where each element is the design freqency at a given pixel coordinate
+design_array=np.ndarray((146,140))
 for i in range(len(design_feedline)):
     for j in range(len(design_feedline[i])):
         design_array[i][j]=design_feedline[i][j]
@@ -63,17 +59,22 @@ for i in range(len(design_feedline)):
         design_array[i][j+7*14]=design_feedline[i][j]
         design_array[i][j+8*14]=design_feedline[i][j]
         design_array[i][j+9*14]=design_feedline[i][j]
+'''
 
+'''
+Written before the feedline class was created, left until feedline class definitively works
+Create an array where each element has the measured values from that
 
-#Create an array where each element has the measured values from that
 final_map_array=np.zeros((len(design_array),len(design_array[0]),5))
 for n in range(len(beammap)):
     final_map_array[int(beammap[n][3])][int(beammap[n][2])][0]=beammap[n][0]
     final_map_array[int(beammap[n][3])][int(beammap[n][2])][2]=beammap[n][1]
     final_map_array[int(beammap[n][3])][int(beammap[n][2])][3]=beammap[n][2]
     final_map_array[int(beammap[n][3])][int(beammap[n][2])][4]=beammap[n][3]
+'''
 
-
+'''
+As above, this was moved to the feedline class
 def AssignFrequency(FinalMap, FreqSweeps):
     counter=0
     array = np.copy(FinalMap)
@@ -92,10 +93,10 @@ def AssignFrequency(FinalMap, FreqSweeps):
             else:
                 array[i][j][1] = 0
     return array,counter
+'''
 
-
-# In[6]:
-
+'''
+Created for the feedline class, will be REMOVED in a further update
 
 good_array,counter=AssignFrequency(final_map_array,FreqSweepFiles)
 
@@ -151,8 +152,13 @@ def get_feedline(model, measured_array,FL_number,flag):
                     measuredfreqs[i][j]=float('NaN')
                 flag_map[i][j]=measured[i][(len(measured[0])-14*fl_num)+j][2]
         return measuredfreqs, flag_map, counter
+'''
 
 
+# This needs to be restructured to account for the data output of the feedline class
+# Additionally should be utilized so that we can perform MCMC on the 'shifting' and 'stretching'
+# parameters in frequency space
+# Essentially, revamp this to turn it into something more like a chi squared function
 def residual_std_dev_finder(residual_map, flag_map, measured_map):
     resids=np.copy(residual_map)
     flags=np.copy(flag_map)
@@ -180,7 +186,8 @@ def residual_std_dev_finder(residual_map, flag_map, measured_map):
 
     return std_dev, resid_vals, average, std_dev_w_outliers, resid_vals_w_outliers, average_with_outliers
 
-
+# Find the range of the frequencies we've measured and then stretch them to be 4 GHz
+# This should be rewritten to take in a stretching parameter so we can vary the stretch
 def frequency_stretcher (frequency_array, minimum_frequency, maximum_frequency):
     min_f=minimum_frequency
     max_f=maximum_frequency
@@ -188,7 +195,7 @@ def frequency_stretcher (frequency_array, minimum_frequency, maximum_frequency):
     stretched_f=norm_f*(4000./(max_f-min_f))
     return stretched_f
 
-
+# Unworked, little tested, must be further developed
 def frequency_shifter(streched_array,design_array):
     shift_array=np.zeros(((len(streched_array),len(streched_array[0]),2)))
     d_array = np.copy(design_array)
