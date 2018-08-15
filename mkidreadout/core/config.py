13dcf1f830c659c67227b8514f50bfa9208360e9
Jeb Bailey <baileyji@umich.edu>
2018-08-14 17:01:39 -0700

    More config work

--- mkidreadout/core/config.py
+++ mkidreadout/core/config.py
@@ -1,26 +1,29 @@
-import ruamel.yaml
+import re
+
+import ruamel.yaml, StringIO
 from mkidreadout.core import caller_name
+from mkidreadout.core.utils import freeze
 from mkidpipeline.core.corelog import getLogger, setup_logging
 try:
     import ConfigParser as configparser
 except ImportError:
     import configparser
 
-RESERVED = ('._c', '._lc', '._t', '._a')
+RESERVED = ('._c', '._a')
 
 yaml = ruamel.yaml.YAML()
 
 setup_logging()
 
-
 @ruamel.yaml.yaml_object(yaml)
 class ConfigDict(dict):
     yaml_tag = u'!configdict'
-
+    __frozen = False
     def __init__(self, *args):
         """ If initialized with a list of tuples cannonization is not enforced on values"""
         if args:
-            self.update([(cannonizekey(k), v) for k, v in args[0]])
+            super(ConfigDict, self).update([(cannonizekey(k), v) for k, v in args[0]])
+        self.__frozen = True
 
     @classmethod
     def to_yaml(cls, representer, node):
@@ -34,11 +37,62 @@ class ConfigDict(dict):
         d = loader.construct_pairs(node)  #WTH this one line took half a day to get right
         return cls(d)
 
+    def dump(self):
+        """Dump the config to a YAML string"""
+        out = StringIO.StringIO()
+        yaml.dump(self, out)
+        return out.getvalue()
+
     def __getattr__(self, key):
+        """This implements dot notation of the config tree without default inheritance
+
+        In a.b.c Python resolves a.b by calling this function then calls .c on the return of a.b,
+        so it is nontrivial for the a.b code to trap any KeyError that would be raised in looking for .c
+        and thus silently returning a.c. This is probably for the best as it makes the intent more
+        explicit. Regardless no config inheritence when accesing with dot notation
+        """
         k1, _, krest = key.partition('.')
         return self[k1][krest] if krest else self[k1]
 
+    def __setattr__(self, key, value):
+        if self.__frozen and not hasattr(self, key):
+            raise AttributeError('Use register to add config attributes')
+        else:
+            object.__setattr__(self, key, value)
+
+    def get(self, name, default=None, inherit=True):
+        """This implements do notation of the config tree with optional inheritance and optional
+        default value. Default values other than None take precedence over inheritance.
+
+        Inheritance means that if cfg.beam.sweep.imgdir doesn't exist but
+        cfg.beam.imgdir (or then cfg.imgdir) would be returned provided they are leafs
+        """
+        k1, _, krest = name.partition('.')
+
+        getLogger('mkidcore.config').debug('{}:{}'.format(name, krest))
+
+        try:
+            next = self[k1]
+        except KeyError as e:
+            if default is None:
+                raise e
+            else:
+                next = default
+
+        if not krest:
+            return next
+
+        try:
+            return next.get(krest, default, inherit)
+        except KeyError as e:
+            if default is not None:
+                return default
+            if inherit:
+                return self[krest.rpartition('.')[2]]
+            raise e
+
     def __contains__(self, k):
+        """Contains only implements explicit keys, inheritance is not checked."""
         k1, _, krest = k.partition('.')
         if super(ConfigDict, self).__contains__(k1):
             if krest:
@@ -62,28 +116,36 @@ class ConfigDict(dict):
                 return False
         return True
 
-    def updateSetting(self, key, value, comment=None, longcomment=None):
-        self.registered(key, error=True)
-        self._update(key, value, comment=comment, longcomment=longcomment)
+    def keys(self):
+        """Hide reserved keys"""
+        return filter(lambda x: not (isinstance(x,str) and x.endswith(RESERVED)),
+                      super(ConfigDict, self).keys())
 
-    def _update(self, key, value, comment=None, longcomment=None):
+    def items(self):
+        """Hide reserved keys"""
+        return filter(lambda x: not (isinstance(x[0],str) and x[0].endswith(RESERVED)),
+                      super(ConfigDict, self).items())
+
+    def update(self, key, value, comment=None):
+        self.registered(key, error=True)
+        self._update(key, value, comment=comment)
+
+    def _update(self, key, value, comment=None, ):
         k1, _, krest = key.partition('.')
 
         if krest:
-            self[k1]._update(krest, value, comment=comment, longcomment=longcomment)
+            self[k1]._update(krest, value, comment=comment)
         else:
             # TODO add checking against allowed
             self[k1] = value
             if comment is not None:
                 self._dict[k1+'._c'] = comment
-            if longcomment is not None:
-                self._dict[k1+'._lc'] = longcomment
 
-    def _register(self, key, initialvalue, type=None, allowed=None, comment='', longcomment=''):
+    def _register(self, key, initialvalue, allowed=None, comment=None):
         k1, _, krest = key.partition('.')
         if krest:
             cd = self.get(k1, ConfigDict())
-            cd._register(krest, initialvalue, type=type, allowed=allowed, comment=comment, longcomment=longcomment)
+            cd._register(krest, initialvalue, allowed=allowed, comment=comment)
             self[k1] = cd
             # getLogger('MKIDConfig').debug('registering {}.{}={}'.format(k1,krest, initialvalue))
         else:
@@ -91,23 +153,41 @@ class ConfigDict(dict):
             self[k1] = initialvalue
             if comment:
                 self[key + '._c'] = comment
-            if longcomment:
-                self[key + '._lc'] = longcomment
-            if type is not None:
-                self[key + '._t'] = type
             if allowed is not None:
                 self[key + '._a'] = allowed
         return self
 
-    def register(self, key, initialvalue, type=None, allowed=None, comment='', longcomment='', update=False):
+    def comment(self, key):
+        self.keyisvalid(key, error=True)
+        self.registered(key, error=True)
+        try:
+            return self[key+'._c']
+        except KeyError:
+            return None
+
+    def register(self, key, initialvalue, allowed=None, comment=None, update=False):
         """Registers a key, true iff the key was registered. Does not update an existing key unless
         update is True."""
         self.keyisvalid(key, error=True)
         ret = not self.registered(key)
         if not ret and not update:
             return ret
-        self._register(key, initialvalue, type=type, allowed=allowed, comment=comment, longcomment=longcomment)
+        self._register(key, initialvalue, allowed=allowed, comment=comment)
         return ret
+
+    def registersubdict(self, key, configdict):
+        self[key] = configdict
+
+    def unregister(self, key):
+        self.keyisvalid(key, error=True)
+        if '.' in key:
+            root,_,end = key.rpartition('.')
+            d = self.get(root, {})
+            for k in [end]+[end+r for r in RESERVED]:
+                d.pop(k, None)
+        else:
+            for k in [key]+[key+r for r in RESERVED]:
+                self.pop(k, None)
 
     def todict(self):
         ret = dict(self)
@@ -117,7 +197,8 @@ class ConfigDict(dict):
         return ret
 
     def save(self, file):
-        yaml.dump(self, file)
+        with open(file,'w') as f:
+            yaml.dump(self, f)
 
     def registerfromconfigparser(self, cp, namespace=None):
         """loads all data in the config parser object, overwriting any that already exist"""
@@ -131,6 +212,7 @@ class ConfigDict(dict):
             ns = namespace + s + '.'
             for k, v in cp.items(s):
                 self.register(cannonizekey(ns + k), cannonizevalue(v), update=True)
+        return self
 
     def registerfromkvlist(self, kv, namespace=None):
         """loads all data in the keyvalue iterable, overwriting any that already exist"""
@@ -142,6 +224,7 @@ class ConfigDict(dict):
             self.register(cannonizekey(namespace + k), cannonizevalue(v), update=True)
         return self
 
+yaml.register_class(ConfigDict)
 
 def cannonizekey(k):
     """Enforce cannonicity of config keys lowercase, no spaces (replace with underscore)"""
@@ -181,6 +264,12 @@ def importoldconfig(config, cfgfile, namespace=None):
     configfiles any collisions will be handled silently by adopting the value
     of the most recently loaded config file. All settings are imported as strings.
     """
+    cp = loadoldconfig(cfgfile)
+    config.registerfromconfigparser(cp, namespace)
+
+
+def loadoldconfig(cfgfile):
+    """Get a configparser instance from an old-style config, including files that were handled by readdict"""
     cp = configparser.ConfigParser()
     try:
         cp.read(cfgfile)
@@ -194,15 +283,62 @@ def importoldconfig(config, cfgfile, namespace=None):
             if not k.strip():
                 continue
             cp.set('DEFAULT', k.strip(), v.strip())
+    return cp
 
-    config.registerfromconfigparser(cp, namespace)
+
+def _consoladateconfig(cd):
+    """Merge Roach and sweep sections into objects"""
+    roaches, sweeps = {}, []
+    for k in cd.keys():
+        if 'roach_' in k:
+            _, _, rnum = k.partition('_')
+            cd.register('roachnum',rnum)
+            roaches[rnum] = cd[k]
+            cd.unregister(k)
+        if re.match('sweep\d+', k):
+            sweeps.append(cd[k])
+            print cd[k]
+            cd[k].register('num', int(k[5:]))
+            cd.unregister(k)
+    if roaches:
+        assert 'roaches' not in cd
+        cd.register('roaches', roaches)
+    if sweeps:
+        assert 'sweeps' not in cd
+        cd.register('sweeps', sweeps)
+
+
+def load(file, namespace=None):
+    if file.lower().endswith(('yaml','yml')):
+        with open(file,'r') as f:
+            return yaml.load(f)
+    elif namespace is None:
+        raise ValueError('Namespace required when loading an old config')
+    else:
+        return ConfigDict().registerfromconfigparser(loadoldconfig(file), namespace)
+
+def ingestoldconfigs(cfiles=('beammap.align.cfg', 'beammap.clean.cfg', 'beammap.sweep.cfg', 'dashboard.cfg',
+                             'initgui.cfg', 'powersweep.ml.cfg',  'templar.cfg')):
+    config = ConfigDict()
+    for cf in cfiles:
+        cp = loadoldconfig(cf)
+        config.registerfromconfigparser(cp, cf[:-4])
+
+    _consoladateconfig(config.beammap.sweep)
+    _consoladateconfig(config.templar)
+    _consoladateconfig(config.initgui)
+    _consoladateconfig(config.dashboard)
+
+    return config
 
 
 config = ConfigDict()
 
-def load(file):
-    with open(file,'r') as f:
-        return yaml.load(f)
+# c = config = ingestoldconfigs()
+
+
+
+#TODO test .c .lc, .a
 
 # #---------------------
 #
---
+++ mkidreadout/core/utils.py
@@ -0,0 +1,24 @@
+from functools import wraps
+
+
+def freeze(cls, msg="Class {cls} is frozen. Cannot set {key} = {value}"):
+    """Wrap and freeze a class so that a=A(); a.foo=4 fails if .foo isn't defined by the class """
+    cls.__frozen = False
+
+    def frozensetattr(self, key, value):
+        if self.__frozen and not hasattr(self, key):
+            raise AttributeError(msg.format(cls=cls.__name__, key=key, value=value))
+        else:
+            object.__setattr__(self, key, value)
+
+    def init_decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            func(self, *args, **kwargs)
+            self.__frozen = True
+        return wrapper
+
+    cls.__setattr__ = frozensetattr
+    cls.__init__ = init_decorator(cls.__init__)
+
+    return cls
