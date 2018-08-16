4015ecbfa45fdacec0ffca9ad3008c11ce3ecce6
Neelay <neelay@dark.physics.ucsb.edu>
2018-08-15 17:23:48 -0700

    Moved cleaning code into class

--- mkidreadout/configuration/beammap/cleanV2.py
+++ mkidreadout/configuration/beammap/cleanV2.py
@@ -58,106 +58,142 @@ def getOverlapGrid(xCoords, yCoords, flags, nXPix, nYPix):
             bmGrid[x, y] += 1
     return bmGrid
 
-def resolveOverlaps(preciseXs, preciseYs, flags, bmGrid, flip=False):
-    flags = np.copy(flags)
-    bmGrid = np.copy(bmGrid)
-    overlapCoords = np.asarray(np.where(bmGrid > 1)).T
-    unoccupiedCoords = np.asarray(np.where(bmGrid==0)).T
 
-    flooredXs = preciseXs.astype(np.int)
-    flooredYs = preciseYs.astype(np.int)
 
-    placedXs = np.floor(preciseXs)
-    placedYs = np.floor(preciseYs)
+class BMCleaner:
+    def __init__(self, roughBMData, nRows, nCols, flip, instrument):
+        self.nRows = int(configData['numRows'])
+        self.nCols = int(configData['numCols'])
+        self.flip = bool(configData['flip'])
+        self.roughBMFile = str(configData['roughBMFile'])
+        self.finalBMFile = str(configData['finalBMFile'])
+        self.beammapDirectory = str(configData['beammapDirectory'])
+        self.instrument = str(configData['instrument']).lower()
 
-    nOverlapsResolved = 0
-    # resolve overlaps out to 1-pixel nearest neighbor
-    for coord in overlapCoords:
-        coordMask = (coord[0] == flooredXs) & (coord[1] == flooredYs) & ((flags == beamMapFlags['good']) | (flags == beamMapFlags['double']))
-        coordInds = np.where(coordMask)[0] #indices of overlapping coordinates in beammap
+        if self.instrument.lower()=='mec':
+            self.nFL = N_FL_MEC
+            self.flWidth = MEC_FL_WIDTH
+        elif self.instrument.lower()=='darkness':
+            self.nFL = N_FL_DARKNESS
+            self.flWidth = DARKNESS_FL_WIDTH
+    
+        self.resIDs = np.array(roughBM[:,0],dtype=np.int)
+        self.flags = np.array(roughBM[:,1],dtype=np.int)
+        self.preciseXs = roughBM[:,2]
+        self.preciseYs = roughBM[:,3]
+        self.flooredXs = None
+        self.flooredYs = None
+        self.placedXs = None
+        self.placedYs = None
+        self.bmGrid = None
 
-        uONNCoords = np.asarray(np.where(bmGrid[coord[0]-1:coord[0]+2, coord[1]-1:coord[1]+2]==0)).T + coord - np.array([1,1])
-        uONNCoords = uONNCoords[isInCorrectFL(resIDs[coordInds[0]]*np.ones(len(uONNCoords)), uONNCoords[0], uONNCoords[1], flip), :]
-            
+    def fixPreciseCoordinates(self):
+        #fix coordinates
+        self._fixInitialFeedlinePlacement()
 
-        precXOverlap = preciseXs[coordMask] - 0.5
-        precYOverlap = preciseYs[coordMask] - 0.5
-        precOverlapCoords = np.array(zip(precXOverlap, precYOverlap)) #list of precise coordinates overlapping with coord
+        #lock in coordinates, set up placement tools/arrays
+        self.flooredXs = self.preciseXs.astype(np.int)
+        self.flooredYs = self.preciseYs.astype(np.int)
+        self.placedXs = np.floor(self.preciseXs)
+        self.placedYs = np.floor(self.preciseYs)
+        self.bmGrid = getOverlapGrid(self.preciseXs, self.preciseYs, self.flags, self.nCols, self.nRows)
 
-        # no nearest neigbors, so pick the closest one and flag the rest as bad
-        if len(uONNCoords)==0:
-            distsFromCenter = (precOverlapCoords[:,0] - coord[0])**2 + (precOverlapCoords[:,1] - coord[1])**2
-            minDistInd = np.argmin(distsFromCenter) #index in precOverlapCoords
-            coordInds = np.delete(coordInds, minDistInd) #remove correct coordinate from overlap
-            bmGrid[coord[0], coord[1]] = 1
-            flags[coordInds] = beamMapFlags['duplicatePixel']
-            placedXs[coordInds] = np.nan
-            placedYs[coordInds] = np.nan
-            continue
+    def placeOnGrid(self):
+        '''
+        Places all resonators on nRowsxnCols size grid. Currently this involves flooring the precise coordinates,
+        resolving overlaps, then randomly placing bad/failed pixels
+        '''
+        self._resolveOverlaps()
+        #self._placeFailedPixels()
 
-        #distMat[i,j] is distance from the ith overlap coord to the jth unoccupied nearest neighbor
-        distMat = np.zeros((len(precOverlapCoords), len(uONNCoords))) 
-        for i in range(len(precOverlapCoords)):
-            distMat[i, :] = (precOverlapCoords[i][0] - uONNCoords[:,0])**2 + (precOverlapCoords[i][1] - uONNCoords[:,1])**2
 
-        for i in range(np.sum(coordMask)-1):
-            minDistInd = np.unravel_index(np.argmin(distMat), distMat.shape)
-            toMoveCoordInd = coordInds[minDistInd[0]] #index in beammap
-            nnToFillCoord = uONNCoords[minDistInd[1]]
+    def _fixInitialFeedlinePlacement(self, slack=0):
+        wayOutMask = ~isInCorrectFL(self.resIDs, self.preciseXs, self.preciseYs, self.instrument, slack, self.flip)
+        self.flags[((self.flags==beamMapFlags['good']) | (self.flags==beamMapFlags['double'])) & wayOutMask] = beamMapFlags['wrongFeedline']
+        self.preciseXs[((self.flags==beamMapFlags['good']) | (self.flags==beamMapFlags['double'])) & wayOutMask] = np.nan
+        self.preciseYs[((self.flags==beamMapFlags['good']) | (self.flags==beamMapFlags['double'])) & wayOutMask] = np.nan
+        log.info('%d pixels in wrong feedline. Flagged as bad', np.sum(self.flags==beamMapFlags['wrongFeedline']))
 
-            placedXs[toMoveCoordInd] = nnToFillCoord[0]
-            placedYs[toMoveCoordInd] = nnToFillCoord[1]
-            bmGrid[nnToFillCoord[0], nnToFillCoord[1]] = 1
-            bmGrid[coord[0], coord[1]] -= 1
+    
+    def _resolveOverlaps(self):
+        '''
+        Resolves overlaps out to one nearest neighbor. Results stored in self.placedXs and self.placedYs. self.bmGrid is also
+        modified.
+        '''
+        overlapCoords = np.asarray(np.where(self.bmGrid > 1)).T
+        unoccupiedCoords = np.asarray(np.where(self.bmGrid==0)).T 
+        nOverlapsResolved = 0
 
-            distMat = np.delete(distMat, minDistInd[1], axis=1) #delete column corresponding to NN just filled
-            distMat = np.delete(distMat, minDistInd[0], axis=0) #delete row corresponding to moved coordinate
-            coordInds = np.delete(coordInds, minDistInd[0])
-            precOverlapCoords = np.delete(precOverlapCoords, minDistInd[0], axis=0)
-            precXOverlap = np.delete(precXOverlap, minDistInd[0])
-            precYOverlap = np.delete(precYOverlap, minDistInd[0])
-
-            nOverlapsResolved += 1
-
-            if distMat.shape[1]==0:
+        for coord in overlapCoords:
+            coordMask = (coord[0] == self.flooredXs) & (coord[1] == self.flooredYs) & ((self.flags == beamMapFlags['good']) | (self.flags == beamMapFlags['double']))
+            coordInds = np.where(coordMask)[0] #indices of overlapping coordinates in beammap
+    
+            uONNCoords = np.asarray(np.where(self.bmGrid[coord[0]-1:coord[0]+2, coord[1]-1:coord[1]+2]==0)).T + coord - np.array([1,1])
+            uONNCoords = uONNCoords[isInCorrectFL(self.resIDs[coordInds[0]]*np.ones(len(uONNCoords)), uONNCoords[:,0], uONNCoords[:,1], self.instrument, self.flip), :]
+                
+    
+            precXOverlap = self.preciseXs[coordMask] - 0.5
+            precYOverlap = self.preciseYs[coordMask] - 0.5
+            precOverlapCoords = np.array(zip(precXOverlap, precYOverlap)) #list of precise coordinates overlapping with coord
+    
+            # no nearest neigbors, so pick the closest one and flag the rest as bad
+            if len(uONNCoords)==0:
                 distsFromCenter = (precOverlapCoords[:,0] - coord[0])**2 + (precOverlapCoords[:,1] - coord[1])**2
                 minDistInd = np.argmin(distsFromCenter) #index in precOverlapCoords
                 coordInds = np.delete(coordInds, minDistInd) #remove correct coordinate from overlap
-                bmGrid[coord[0], coord[1]] = 1
-                flags[coordInds] = beamMapFlags['duplicatePixel']
-                placedXs[coordInds] = np.nan
-                placedYs[coordInds] = np.nan
-                break
-                
-    log.info('Successfully resolved %d overlaps', nOverlapsResolved)
-    log.info('Failed to resolve %d overlaps', np.sum(flags==beamMapFlags['duplicatePixel']))
-
-    return placedXs, placedYs, flags, bmGrid
-
-def placeFailedPixels(resIDs, placedXs, placedYs, flags, bmGrid, instrument, flip):
-    placedXs = np.copy(placedXs)
-    placedYs = np.copy(placedYs)
-    toPlaceMask = np.isnan(placedXs) | np.isnan(placedYs)
-    unoccupiedCoords = np.asarray(np.where(bmGrid==0)).T
-    if instrument.lower()=='mec':
-        nFL = N_FL_MEC
-        flWidth = DARKNESS_FL_WIDTH
-    elif instrument.lower()=='darkness':
-        nFL = N_FL_DARKNESS
-        flWidth = DARKNESS_FL_WIDTH
+                self.bmGrid[coord[0], coord[1]] = 1
+                self.flags[coordInds] = beamMapFlags['duplicatePixel']
+                self.placedXs[coordInds] = np.nan
+                self.placedYs[coordInds] = np.nan
+                continue
     
-    for i in range(nFL):
-        toPlaceMaskCurFL = toPlaceMask & ((resIDs/10000 - 1) == i)
-        unoccupiedCoordsCurFL = unoccupiedCoords[isInCorrectFL(10000*(i+1)*np.ones(len(unoccupiedCoordsCurFL)), unoccupiedCoords[:,0], unoccupiedCoords[:,1], instrument, flip=flip)]
-        placedXs[toPlaceMaskCurFL] = unoccupiedCoordsCurFL[:,0]
-        placedYs[toPlaceMaskCurFL] = unoccupiedCoordsCurFL[:,1]
-
-    return placedXs, placedYs
-
-class BMCleaner(self, roughBMData, configData):
-    pass
-
+            #distMat[i,j] is distance from the ith overlap coord to the jth unoccupied nearest neighbor
+            distMat = np.zeros((len(precOverlapCoords), len(uONNCoords))) 
+            for i in range(len(precOverlapCoords)):
+                distMat[i, :] = (precOverlapCoords[i][0] - uONNCoords[:,0])**2 + (precOverlapCoords[i][1] - uONNCoords[:,1])**2
     
+            for i in range(np.sum(coordMask)-1):
+                minDistInd = np.unravel_index(np.argmin(distMat), distMat.shape)
+                toMoveCoordInd = coordInds[minDistInd[0]] #index in beammap
+                nnToFillCoord = uONNCoords[minDistInd[1]]
+    
+                self.placedXs[toMoveCoordInd] = nnToFillCoord[0]
+                self.placedYs[toMoveCoordInd] = nnToFillCoord[1]
+                self.bmGrid[nnToFillCoord[0], nnToFillCoord[1]] = 1
+                self.bmGrid[coord[0], coord[1]] -= 1
+    
+                distMat = np.delete(distMat, minDistInd[1], axis=1) #delete column corresponding to NN just filled
+                distMat = np.delete(distMat, minDistInd[0], axis=0) #delete row corresponding to moved coordinate
+                coordInds = np.delete(coordInds, minDistInd[0])
+                precOverlapCoords = np.delete(precOverlapCoords, minDistInd[0], axis=0)
+                precXOverlap = np.delete(precXOverlap, minDistInd[0])
+                precYOverlap = np.delete(precYOverlap, minDistInd[0])
+    
+                nOverlapsResolved += 1
+    
+                if distMat.shape[1]==0:
+                    distsFromCenter = (precOverlapCoords[:,0] - coord[0])**2 + (precOverlapCoords[:,1] - coord[1])**2
+                    minDistInd = np.argmin(distsFromCenter) #index in precOverlapCoords
+                    coordInds = np.delete(coordInds, minDistInd) #remove correct coordinate from overlap
+                    self.bmGrid[coord[0], coord[1]] = 1
+                    self.flags[coordInds] = beamMapFlags['duplicatePixel']
+                    self.placedXs[coordInds] = np.nan
+                    self.placedYs[coordInds] = np.nan
+                    break
+                    
+        log.info('Successfully resolved %d overlaps', nOverlapsResolved)
+        log.info('Failed to resolve %d overlaps', np.sum(self.flags==beamMapFlags['duplicatePixel']))
+
+    def _placeFailedPixels(self):
+        toPlaceMask = np.isnan(self.placedXs) | np.isnan(self.placedYs)
+        unoccupiedCoords = np.asarray(np.where(self.bmGrid==0)).T
+        
+        for i in range(self.nFL):
+            toPlaceMaskCurFL = toPlaceMask & ((self.resIDs/10000 - 1) == i)
+            unoccupiedCoordsCurFL = unoccupiedCoords[isInCorrectFL(10000*(i+1)*np.ones(len(unoccupiedCoords)), unoccupiedCoords[:,0], unoccupiedCoords[:,1], self.instrument, flip=self.flip)]
+            self.placedXs[toPlaceMaskCurFL] = unoccupiedCoordsCurFL[:,0]
+            self.placedYs[toPlaceMaskCurFL] = unoccupiedCoordsCurFL[:,1]
+     
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='argument parser')
@@ -170,14 +206,9 @@ if __name__=='__main__':
     # Open config file
     configData = readDict()
     configData.read_from_file(configFileName)
-
-    nRows = int(configData['numRows'])
-    nCols = int(configData['numCols'])
-    flip = bool(configData['flip'])
-    roughBMFile = str(configData['roughBMFile'])
-    finalBMFile = str(configData['finalBMFile'])
-    beammapDirectory = str(configData['beammapDirectory'])
-    instrument = str(configData['instrument']).lower()
+    beammapDirectory = configData['beammapDirectory']
+    roughBMFile = configData['roughBMFile']
+    finalBMFile = configData['finalBMFile']
 
     #put together full input/output BM paths
     roughPath = os.path.join(beammapDirectory,roughBMFile)
@@ -185,46 +216,33 @@ if __name__=='__main__':
     
     #load location data from rough BM file
     roughBM = np.loadtxt(roughPath)
-    ids = np.array(roughBM[:,0],dtype=np.int)
-    flags = np.array(roughBM[:,1],dtype=np.int)
-    preciseXs = roughBM[:,2]
-    preciseYs = roughBM[:,3]
+   
+    cleaner = BMCleaner(roughBM, configData['numRows'], configData['numCols'], configData['flip'], configData['instrument'])
 
     # fix feedline placement
-    wayOutMask = ~isInCorrectFL(ids, preciseXs, preciseYs, instrument, 0, flip)
-    flags[((flags==beamMapFlags['good']) | (flags==beamMapFlags['double'])) & wayOutMask] = beamMapFlags['wrongFeedline']
-    preciseXs[((flags==beamMapFlags['good']) | (flags==beamMapFlags['double'])) & wayOutMask] = np.nan
-    preciseYs[((flags==beamMapFlags['good']) | (flags==beamMapFlags['double'])) & wayOutMask] = np.nan
-    log.info('%d pixels in wrong feedline. Flagged as bad', np.sum(flags==beamMapFlags['wrongFeedline']))
+    cleaner.fixPreciseCoordinates()
 
-    #offByOneMask = (~isInCorrectFL(ids, preciseXs, preciseYs, instrument, 0, flip)) & (~wayOutMask)
-
-    flooredXs = preciseXs.astype(np.int)
-    flooredYs = preciseYs.astype(np.int)
-
-    bmGrid = getOverlapGrid(flooredXs, flooredYs, flags, nCols, nRows)
     #plt.ion()
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
-    ax1.imshow(bmGrid.T)
+    ax1.imshow(cleaner.bmGrid.T)
     ax1.set_title('Initial (floored) beammap (value = res per pix coordinate)')
 
-    #resolve overlaps
-    placedXs, placedYs, flags, bmGrid = resolveOverlaps(preciseXs, preciseYs, flags, bmGrid, flip)
-
+    # resolve overlaps and place failed pixels
+    cleaner.placeOnGrid()
 
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111)
-    ax2.imshow(bmGrid.T)
+    ax2.imshow(cleaner.bmGrid.T)
     ax2.set_title('Beammap after fixing overlaps (value = res per pix coordinate)')
 
     fig3 = plt.figure()
     ax3 = fig3.add_subplot(111)
-    goodPixMask = (flags==beamMapFlags['good']) | (flags==beamMapFlags['double'])
-    placedXsGood = placedXs[goodPixMask]
-    placedYsGood = placedYs[goodPixMask]
-    preciseXsGood = preciseXs[goodPixMask] - 0.5
-    preciseYsGood = preciseYs[goodPixMask] - 0.5
+    goodPixMask = (cleaner.flags==beamMapFlags['good']) | (cleaner.flags==beamMapFlags['double'])
+    placedXsGood = cleaner.placedXs[goodPixMask]
+    placedYsGood = cleaner.placedYs[goodPixMask]
+    preciseXsGood = cleaner.preciseXs[goodPixMask] - 0.5
+    preciseYsGood = cleaner.preciseYs[goodPixMask] - 0.5
     print(placedXsGood - preciseXsGood)
 
     #u = np.zeros((nRows, nCols))
@@ -245,14 +263,7 @@ if __name__=='__main__':
     ax4 = fig4.add_subplot(111)
     smallVMask = ((placedXsGood - preciseXsGood) < 0.5) & ((placedYsGood - preciseYsGood) < 0.5)
     ax4.quiver(placedXsGood[smallVMask], placedYsGood[smallVMask], placedXsGood[smallVMask] - preciseXsGood[smallVMask], placedYsGood[smallVMask] - preciseYsGood[smallVMask], angles='xy', scale_units='xy', scale=None, width=0.001)
-    ax4.set_title('Quiver Plot showing final_coordinates -> precise_coordinates (arrows not to scale)')
-
-    #randomly place failed pixels
-    placedXs, placedYs = placeFailedPixels(resIDs, placedXs, placedYs, flags, bmGrid, instrument, flip)
-
-
-
-    
+    ax4.set_title('Quiver Plot showing final_coordinates -> precise_coordinates (arrows not to scale)')    
 
     plt.show()
             
