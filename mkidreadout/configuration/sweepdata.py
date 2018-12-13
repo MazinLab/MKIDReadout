5d11ca8ef8ed617f0a0e954bd99a0fbd5fa08554
Jeb Bailey <baileyji@umich.edu>
2018-12-12 16:28:01 -0800

    Create function sweepdata.loadold() function to convert old widesweep files into new ones

--- mkidreadout/configuration/sweepdata.py
+++ mkidreadout/configuration/sweepdata.py
@@ -1,15 +1,16 @@
 import numpy as np
-
+import os
 
 ISGOOD = 1
 ISBAD = 0
 
+
 class SweepMetadata(object):
-    def __init__(self, feedline, resid=None, wsfreq=None, flag=None, mlfreq=None, atten=None,
+    def __init__(self, resid=None, wsfreq=None, flag=None, mlfreq=None, atten=None,
                  ml_isgood_score=None, ml_isbad_score=None, file=''):
 
         self.file = file
-        self.feedline = feedline
+        self.feedline = None
 
         self.resIDs = resid
         self.wsfreq = wsfreq
@@ -38,6 +39,7 @@ class SweepMetadata(object):
         assert (self.resIDs.size==self.wsfreq.size==self.flag.size==
                 self.atten.size==self.mlfreq.size==self.ml_isgood_score.size==self.ml_isbad_score.size)
 
+        self._settypes()
         self.sort()
 
     def sort(self):
@@ -55,12 +57,34 @@ class SweepMetadata(object):
                          self.ml_isbad_score])
 
     def save(self, file=''):
-        sf = file if file else self.file
+        sf = file.format(feedline=self.feedline) if file else self.file.format(feedline=self.feedline)
         np.savetxt(sf, self.toarray().T, fmt="%8d %1u %16.7f %16.7f %5.1f %6.4f %6.4f",
                    header='feedline={}\nrID\trFlag\twsFreq\tmlFreq\tatten\tmlGood\tmlBad'.format(self.feedline))
 
-    def _load(self):
-        d = np.loadtxt(self.file, unpack=True)
-        self.resIDs, self.flag, self.wsfreq, self.mlfreq, self.atten, self.ml_isgood_score, self.ml_isbad_score = d
+    def _settypes(self):
         self.flag = self.flag.astype(int)
         self.resIDs = self.resIDs.astype(int)
+        self.feedline = int(self.resIDs[0]/10000)
+
+    def _load(self):
+        d = np.loadtxt(self.file.format(feedline=self.feedline), unpack=True)
+        self.resIDs, self.flag, self.wsfreq, self.mlfreq, self.atten, self.ml_isgood_score, self.ml_isbad_score = d
+        self._settypes()
+
+goodfile = '/Users/one/ucsb/Packages/MKIDReadout/mkidreadout/configuration/powersweep/digWS_FL5-freqs-good.txt'
+allfile = '/Users/one/ucsb/Packages/MKIDReadout/mkidreadout/configuration/powersweep/digWS_FL5-freqs-all.txt'
+
+def loadold(allfile, goodfile, outfile='digWS_FL{feedline}_metadata.txt'):
+    gid, gndx, gfreq = np.loadtxt(goodfile, unpack=True)
+    aid, andx, afreq = np.loadtxt(allfile, unpack=True)
+
+    flags = np.full(aid.size, ISBAD)
+
+    assert np.unique(gid).size == gid.size and np.unique(aid).size == aid.size
+
+    badids = np.setdiff1d(aid, gid)
+    bad = np.isin(aid, badids)
+
+    flags[~bad] = ISGOOD
+
+    return SweepMetadata(resid=aid, wsfreq=afreq, flag=flags, file=outfile)
