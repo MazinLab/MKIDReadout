382c43caa84d95a54bc926600337df08bb6b6d69
abwalter <abwalter@physics.ucsb.edu>
2018-12-12 17:24:09 -1000

    Made maxAttens run  parallel

--- mkidreadout/channelizer/maxAttens.py
+++ mkidreadout/channelizer/maxAttens.py
@@ -8,18 +8,27 @@ Usage: $python maxAttens.py 222 223
 """
 
 import sys
+import threading
 from mkidreadout.channelizer.Roach2Controls import Roach2Controls
 
-def maxAttens(roachNums=[]):
-    for r in roachNums:
-        ip = '10.0.0.'+str(r)
-        params = '/home/mecvnc/MKIDReadout/mkidreadout/channelizer/darknessfpga.param'
-        roach = Roach2Controls(ip, params, True)
-        roach.connect()
-        roach.changeAtten(0, 31.75)
-        roach.changeAtten(1, 31.75)
-        roach.changeAtten(2, 31.75)
-        roach.changeAtten(3, 31.75)
+def worker(rNum, params='darknessfpga.param'):
+    ip = '10.0.0.'+str(rNum)
+    #params = '/home/mecvnc/MKIDReadout/mkidreadout/channelizer/darknessfpga.param'
+    roach = Roach2Controls(ip, params, True)
+    roach.connect()
+    roach.changeAtten(0, 31.75)
+    roach.changeAtten(1, 31.75)
+    roach.changeAtten(2, 31.75)
+    roach.changeAtten(3, 31.75)
+
+def maxAttens(roachNums, params='darknessfpga.param'):
+    threads = []
+    for rNum in roachNums:
+        t=threading.Thread(target=worker, args=(rNum,params,))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()    #block until they complete
 
 
 if __name__=='__main__':
