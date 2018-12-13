84187ef80ccd7d6b9c48e92f1664ced9adc99823
Jeb Bailey <baileyji@umich.edu>
2018-12-13 14:50:40 -0800

    bugfix and sensibility check

--- mkidreadout/configuration/sweepdata.py
+++ mkidreadout/configuration/sweepdata.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mkidcore.corelog import getLogger
+
 
 ISGOOD = 1
 ISBAD = 0
@@ -95,7 +97,7 @@ class SweepMetadata(object):
         return self.ml_isgood_score - self.ml_isbad_score
 
     def plot_scores(self):
-        plt.hist(netscore, np.linspace(-1,1,50), label='netscore')
+        plt.hist(self.netscore, np.linspace(-1, 1, 50), label='netscore')
 
     def sort(self):
         s = np.argsort(self.wsfreq)
@@ -113,6 +115,14 @@ class SweepMetadata(object):
 
     def save(self, file=''):
         sf = file.format(feedline=self.feedline) if file else self.file.format(feedline=self.feedline)
+
+        if (np.abs(self.atten[~np.isnan(self.atten)])>100).any():
+            getLogger(__name__).warning('odd attens')
+        if (np.abs(self.ml_isgood_score[~np.isnan(self.ml_isgood_score)])>1).any():
+            getLogger(__name__).warning('bad ml good score')
+        if (np.abs(self.ml_isbad_score[~np.isnan(self.ml_isbad_score)]) > 1).any():
+            getLogger(__name__).warning('bad ml bad scores')
+
         np.savetxt(sf, self.toarray().T, fmt="%8d %1u %16.7f %16.7f %5.1f %6.4f %6.4f",
                    header='feedline={}\nrID\trFlag\twsFreq\tmlFreq\tatten\tmlGood\tmlBad'.format(self.feedline))
 
