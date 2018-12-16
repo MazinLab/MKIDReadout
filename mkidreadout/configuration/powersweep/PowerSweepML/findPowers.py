c096aab79559d09c82e8d7b03e4b3af6e74ad4e3
Neelay <neelay@dark.physics.ucsb.edu>
2018-12-15 19:43:01 -0800

    minor changes to findPowers.py

--- mkidreadout/configuration/powersweep/PowerSweepML/findPowers.py
+++ mkidreadout/configuration/powersweep/PowerSweepML/findPowers.py
@@ -10,6 +10,7 @@ Usage: python findPowers.py <mlConfigFile> <h5File>
 '''
 import numpy as np
 import tensorflow as tf
+import scipy.integrate
 import os, sys
 import argparse
 from PSFitMLData import PSFitMLData
@@ -91,10 +92,10 @@ def findPowers(mlDict, mlBadDict, psDataFileName, metadataFn=None, saveScores=Fa
         if FREQ_USE_MAG:
             inferenceData.opt_freqs[rn] = freqCube[iAtt, np.argmin(image[iAtt, :, 3])] #TODO: make this more robust
         else:
-            #inferenceData.opt_freqs[rn] = freqCube[iAtt, np.argmax(np.correlate(image[iAtt, :, 2], np.ones(5), 'same'))] #TODO: make this more robust
-            use = np.abs(freqCube[iAtt] - inferenceData.initfreqs[rn])<250.e3
-            iqvIntegral = scipy.integrate.trapz(image[iAtt, use, 2]*freqCube[iAtt, use], dx=(freqCube[iAtt, 1] - freqCube[iAtt, 0]))
-            inferenceData.opt_freqs[rn] = iqvIntegral/scipy.integrate.trapz(image[iAtt, use, 2], dx=(freqCube[iAtt, 1] - freqCube[iAtt, 0]))
+            inferenceData.opt_freqs[rn] = freqCube[iAtt, np.argmax(np.correlate(image[iAtt, :, 2], np.ones(5), 'same'))] #TODO: make this more robust
+            #use = np.abs(freqCube[iAtt] - inferenceData.initfreqs[rn])<250.e3
+            #iqvIntegral = scipy.integrate.trapz(image[iAtt, use, 2]*freqCube[iAtt, use], dx=(freqCube[iAtt, 1] - freqCube[iAtt, 0]))
+            #inferenceData.opt_freqs[rn] = iqvIntegral/scipy.integrate.trapz(image[iAtt, use, 2], dx=(freqCube[iAtt, 1] - freqCube[iAtt, 0]))
             
         assert inferenceData.freqs[rn,0] <= inferenceData.opt_freqs[rn] <= inferenceData.freqs[rn,-1], 'freq out of range, need to debug'
         inferenceData.scores[rn] = inferenceLabels[rn, iAtt]
@@ -113,7 +114,8 @@ def findPowers(mlDict, mlBadDict, psDataFileName, metadataFn=None, saveScores=Fa
     if psDataFileName.split('.')[1]=='h5':
         inferenceData.savePSTxtFile(flag = '_' + mlDict['modelName'],outputFN=None, saveScores=saveScores)
     elif psDataFileName.split('.')[1]=='npz':
-        inferenceData.saveInferenceData(flag = '_' +mlDict['modelName'])
+        #inferenceData.saveInferenceData(flag = '_' +mlDict['modelName'])
+        inferenceData.saveInferenceData(flag = '')
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='ML Inference Script')
