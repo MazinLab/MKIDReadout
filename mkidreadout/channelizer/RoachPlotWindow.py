932de347a847684fd97aa3b018698f2cca4198a1
Jeb Bailey <baileyji@umich.edu>
2019-01-12 00:35:53 -0800

    bugfix

--- mkidreadout/channelizer/RoachPlotWindow.py
+++ mkidreadout/channelizer/RoachPlotWindow.py
@@ -845,7 +845,7 @@ class RoachSweepWindow(QMainWindow):
         freqFile = self.roach.roachController.tagfile(self.config.roaches.get('r{}.freqfileroot'.format(self.roachNum)),
                                                       dir=self.config.paths.data)
         sd = sweepdata.SweepMetadata(file=freqFile)
-        sd.file = '{0}_new.{1}'.format(*freqFile.lrpartition('.')[::2])
+        sd.file = '{0}_new.{1}'.format(*freqFile.rpartition('.')[::2])
         lo = self.config.roaches.get('r{}.lo_freq'.format(self.roachNum))
         sd.update_from_roach(lo, freqs=freqs, attens=attens)
         getLogger(__name__).info("Saving %s", sd)
