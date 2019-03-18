669c400a11ff8a4701370e8f58cff3fb9eeb87af
Jeb Bailey <baileyji@umich.edu>
2019-03-18 13:37:18 -0700

    add conex2pixel

--- mkidreadout/hardware/conex.py
+++ mkidreadout/hardware/conex.py
@@ -60,6 +60,74 @@ conex_fields = {
 }
 
 
+def CONEX2PIXEL(xCon, yCon):
+    """ Emprically determined """
+    # from scipy.optimize import curve_fit
+    # xCon0 = -0.035
+    # xCon1 = 0.23
+    # xCon2 = 0.495
+    #
+    # yCon0 = -0.76
+    # yCon1 = -0.38
+    # yCon2 = 0.0
+    # yCon3 = 0.38
+    #
+    # xPos0array = np.array([125.46351537124369, 124.79156638384541])
+    # xPos1array = np.array([107.98640545380867, 106.53992257621843, 106.04177093203712])
+    # xPos2array = np.array([93.809781273378277, 93.586178673966316, 91.514837557492427, 89.872003744327927])
+    #
+    # yPos0array = np.array([36.537397207881689])
+    # yPos1array = np.array([61.297923464154792, 61.535802615842933, 61.223871938056725])
+    # yPos2array = np.array([88.127237564834743, 90.773675516601259, 90.851982786156569])
+    # yPos3array = np.array([114.66071882865981, 115.42948957872515])
+    #
+    # xPos0 = np.median(xPos0array)
+    # xPos1 = np.median(xPos1array)
+    # xPos2 = np.median(xPos2array)
+    #
+    # yPos0 = np.median(yPos0array)
+    # yPos1 = np.median(yPos1array)
+    # yPos2 = np.median(yPos2array)
+    # yPos3 = np.median(yPos3array)
+    #
+    # xPos0err = np.std(xPos0array)
+    # xPos1err = np.std(xPos1array)
+    # xPos2err = np.std(xPos2array)
+    #
+    # yPos0err = np.std(yPos0array)
+    # yPos1err = np.std(yPos1array)
+    # yPos2err = np.std(yPos2array)
+    # yPos3err = np.std(yPos3array)
+    #
+    # xConFit = np.array([xCon0, xCon1, xCon2])
+    # xPosFit = np.array([xPos0, xPos1, xPos2])
+    # xPoserrFit = np.array([xPos0err, xPos1err, xPos2err])
+    #
+    # yConFit = np.array([yCon0, yCon1, yCon2, yCon3])
+    # yPosFit = np.array([yPos0, yPos1, yPos2, yPos3])
+    # yPoserrFit = np.array([np.sqrt(yPos0array[0]), yPos1err, yPos2err, yPos3err])
+
+    def func(x, slope, intercept):
+        return x * slope + intercept
+
+    # xopt, xcov = curve_fit(func, xConFit, xPosFit, sigma=xPoserrFit)
+    # yopt, ycov = curve_fit(func, yConFit, yPosFit, sigma=yPoserrFit)
+
+    xopt = np.array([-65.43754816, 122.74171408])
+    xcov = np.array([[21.81030773, -0.4446162 ],
+                     [-0.4446162 ,  0.37222303]])
+    yopt = np.array([70.84760994, 88.23558729])
+    ycov = np.array([[0.5971835 , 0.17673413],
+                     [0.17673413, 0.08550255]])
+
+    def con2Pix(xCon, yCon, func):
+        return [func(xCon, *xopt), func(yCon, *yopt)]
+
+    xPos, yPos = con2Pix(xCon, yCon, func)
+
+    return [-xPos, -yPos]
+
+
 class Conex(object):
     def __init__(self, port="COM9", baudrate=912600, bytesize=serial.EIGHTBITS,
                  stopbits=serial.STOPBITS_ONE, timeout=1., xonxoff=True, controllerNum=1):
