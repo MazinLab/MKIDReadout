dedac10ea4bc0d9f2ed92590c8eda87fbe16c685
Jeb Bailey <baileyji@umich.edu>
2019-04-25 16:26:57 -0700

    238 & 239 are offline

--- mkidreadout/instruments.py
+++ mkidreadout/instruments.py
@@ -8,7 +8,7 @@ DARKNESS_FEEDLINE_INFO = dict(num=5, width=25, length=80)
 DEFAULT_ARRAY_SIZES = {'mec': (140, 146), 'darkness': (80, 125)}
 
 
-MEC_NUM_FL_MAP = {236: '1a', 237: '1b', 238: '5a', 239: '5b',
+MEC_NUM_FL_MAP = {236: '1a', 237: '1b', #238: '5a', 239: '5b',
                   220: '6a', 221: '6b', 222: '7a', 223: '7b', 232: '8a',
                   233: '8b', 228: '9a', 229: '9b', 224: '10a', 225: '10b'}
 
