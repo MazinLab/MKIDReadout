544065c91e18e96b4687e4b4783c6fd5880daf8a
Neelay Fruitwala <neelay@localhost.localdomain>
2019-06-03 15:50:55 -1000

    Changed compile command

--- mkidreadout/resources/firmware/eventListParser.c
+++ mkidreadout/resources/firmware/eventListParser.c
@@ -5,18 +5,22 @@
 
 #include <mkidshm.h>
 
-// gcc eventListParser.c -o eventListParser -lmkidshm -lpthread -lrt
+// gcc eventListParser.c -o eventListParser -I../../readout/mkidshm -L../../readout/mkidshm -Wl,-rpath=../../readout/mkidshm/ -lmkidshm -lpthread -lrt
 
 int main(){
     MKID_EVENT_BUFFER eventBuffer;
     MKID_PHOTON_EVENT photon;
+    int photonInd=0;
 
     MKIDShmEventBuffer_open(&eventBuffer, "/EventBufferTest0");
 
     while(1){
         sem_wait(eventBuffer.newPhotonSemList[0]);
-        printf("Photon #%d\n", eventBuffer.md->endInd);
-        photon = eventBuffer.eventBuffer[eventBuffer.md->endInd];
+        //photonInd = eventBuffer.md->endInd;
+        photonInd += 1;
+        photonInd = photonInd%eventBuffer.md->bufferSize;
+        printf("Photon #%d\n", photonInd);
+        photon = eventBuffer.eventBuffer[photonInd];
         printf("    x: %d\n", photon.x);
         printf("    y: %d\n", photon.y);
         printf("   ts: %lu\n", photon.time);
