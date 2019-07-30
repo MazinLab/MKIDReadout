534e7673454d9f3e7e2d9cf25592d74cd1bae6df
neelay <neelay@physics.ucsb.edu>
2019-07-30 16:33:33 -0700

    Added power check

--- mkidreadout/configuration/powersweep/ml/checkWPSPerformance.py
+++ mkidreadout/configuration/powersweep/ml/checkWPSPerformance.py
@@ -31,6 +31,13 @@ def matchResonators(manResIDs, mlResIDs, manFreqs, mlFreqs, maxdf=250.e3):
 
     return manToML
 
+def matchAttens(manAttens, mlAttens, manToML):
+    manToMLInds = manToML[~np.isnan(manToML[:, 0]), 0]
+    manAttens = manAttens[~np.isnan(manToML[:, 0])] #mask out manAttens that weren't found
+    mlAttens = mlAttens[manToMLInds.astype(int)]
+    assert len(manAttens)==len(mlAttens)
+    return manAttens, mlAttens
+
     
 
 if __name__=='__main__':
@@ -39,7 +46,7 @@ if __name__=='__main__':
     parser.add_argument('manualFile', help='manual metadata file')
     parser.add_argument('-df', '--max-df', type=float, default=250.e3)
     parser.add_argument('-t', '--threshold', type=float, default=0.5)
-    parser.add_argument('-c', '--cut', type=float)
+    parser.add_argument('-c', '--cut', type=float, default=1.0)
     parser.add_argument('-l', '--lower', default=2.5, type=float)
     parser.add_argument('-u', '--upper', default=2.5, type=float)
     parser.add_argument('-p', '--plotConfusion', action='store_true')
@@ -84,3 +91,51 @@ if __name__=='__main__':
     print 'Not found ResIDs: ', manResIDs[np.isnan(manToML[:, 0])]
 
     print 'False positive ML ResIDs: ', falsePositives
+
+    manAttens, mlAttens = matchAttens(manAttens, mlAttens, manToML)
+    attenDiff = mlAttens - manAttens
+    nLowerBad = np.sum(attenDiff < -1*args.lower)
+    nUpperBad = np.sum(attenDiff > args.upper)
+
+    print nLowerBad, '/', len(attenDiff), '(', 100*nLowerBad/float(len(attenDiff)), '% )', 'Resonators overpowered by', \
+            args.lower, 'dB'
+    print nUpperBad, '/', len(attenDiff), '(', 100*nUpperBad/float(len(attenDiff)), '% )', 'Resonators underpowered by', \
+            args.upper, 'dB'
+    print 'Atten Error stdev', np.std(attenDiff)
+
+    cutMask = mlScores > args.cut
+    cutMask = cutMask[manToML[~np.isnan(manToML[:, 0]), 0].astype(int)]
+    assert len(cutMask) == len(attenDiff)
+    attenDiffCut = attenDiff[cutMask]
+    nLowerBadCut = np.sum(attenDiffCut < -1*args.lower)
+    nUpperBadCut = np.sum(attenDiffCut > args.upper)
+
+    print nLowerBadCut, '/', len(attenDiffCut), '(', 100*nLowerBadCut/float(len(attenDiffCut)), '% )', 'Cut resonators overpowered by', \
+            args.lower, 'dB'
+    print nUpperBadCut, '/', len(attenDiffCut), '(', 100*nUpperBadCut/float(len(attenDiffCut)), '% )', 'Cut resonators underpowered by', \
+            args.upper, 'dB'
+    print 'Atten Error stdev', np.std(attenDiff)
+    
+
+    plt.hist(attenDiff, bins=10, range=(-5,5))
+    plt.show()
+
+    if True:
+        manAttens[manAttens==-1] = max(manAttens)
+        attenStart = min(manAttens)
+        manAttens -= attenStart
+        mlAttens -= attenStart
+        manAttens = np.round(manAttens).astype(int)
+        mlAttens = np.round(mlAttens).astype(int)
+        confImage = np.zeros((max(manAttens) - min(manAttens) + 1, max(manAttens) - min(manAttens) + 1))
+        for i in range(len(manAttens)):
+            confImage[manAttens[i], mlAttens[i]] += 1
+    
+        plt.imshow(np.transpose(confImage), vmax=30)
+        plt.xlabel('True Atten')
+        plt.ylabel('Guess Atten')
+        title = os.path.basename(args.mlFile).split('.')[0] #args.mlFiles[0].split('_')[-1].split('.')[0]
+        plt.title(title)
+        plt.colorbar()
+        plt.savefig(os.path.join(os.path.dirname(args.mlFile), title+'.png'))
+        plt.show()
