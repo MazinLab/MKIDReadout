35813589907a21f2552328d30a6ecbab0267e3d9
neelay <neelay@physics.ucsb.edu>
2019-12-04 15:23:20 -0800

    LO script debugging, now works w/ one board

--- mkidreadout/configuration/findLOsAndMakeFreqLists.py
+++ mkidreadout/configuration/findLOsAndMakeFreqLists.py
@@ -16,7 +16,7 @@ import mkidcore.config
 import mkidreadout.configuration.sweepdata as sd
 
 
-def findLOs(freqsA, freqsB, sweepLOA, sweepLOB, loRange=10.e6, nIters=10000, colParamWeight=1, resBW=200.e3, ifHole=3.e6):
+def findLOs(freqsA, sweepLOA, freqsB=None, sweepLOB=None, loRange=10.e6, nIters=10000, colParamWeight=1, resBW=200.e3, ifHole=3.e6):
     '''
     Finds the optimal LO frequencies for a feedline, given a list of resonator frequencies.
     Does Monte Carlo optimization to minimize the number of out of band tones and sideband 
@@ -41,6 +41,11 @@ def findLOs(freqsA, freqsB, sweepLOA, sweepLOB, loRange=10.e6, nIters=10000, col
     -------
         lo1, lo2 - low and high frequency LOs (in Hz)
     '''
+    if sweepLOB is None:
+        sweepLOB = sweepLOA + 2.e9 + 2*loRange
+    if freqsB is None:
+        freqsB = np.array([sweepLOB - 50.e6, sweepLOB + 60.e6])
+
     lfRange = np.array([sweepLOA - loRange/2., sweepLOA + loRange/2.])
     hfRange = np.array([sweepLOB - loRange/2., sweepLOB + loRange/2.])
     
@@ -92,24 +97,41 @@ def getRoachNumberFromFile(fn):
     return int(re.findall('\d{3}', fn)[0])
 
 if __name__=='__main__':
-    parser = ArgumentParser()
-    parser.add_argument('sweep_data', nargs=2, help='Sweep npz files for a feedline (LF then HF)')
-    parser.add_argument('-m', '--metadata', nargs=2, help='Sweep metadata files')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sweep_data', nargs='+', help='Sweep npz files for a feedline (LF then HF)')
+    parser.add_argument('-m', '--metadata', nargs='+', help='Sweep metadata files')
     parser.add_argument('-c', '--config', help='roach.yml file to modify. WILL BE OVERWRITTEN.')
     args = parser.parse_args()
 
+    if len(args.sweep_data) != len(args.metadata):
+        raise Exception('Must specify metadata/sweep file for every board')
+
+    if len(args.sweep_data) > 2:
+        raise Exception('Sorry, only one FL at a time for now :(')
+
     sweepA = sd.FreqSweep(args.sweep_data[0])
-    sweepB = sd.FreqSweep(args.sweep_data[1])
-    mdA = sd.FreqSweep(args.metadata[0])
-    mdB = sd.FreqSweep(args.metadata[1])
-    freqsA = mdA.freqs[(mdA.flag & sd.IS_GOOD) == sd.IS_GOOD]
-    freqsB = mdB.freqs[(mdB.flag & sd.IS_GOOD) == sd.IS_GOOD]
-    loA, loB = findLOs(freqsA, freqsB, sweepA.lo, sweepB.lo)
+    mdA = sd.SweepMetadata(file=args.metadata[0])
+    freqsA = mdA.freq[(mdA.flag & sd.ISGOOD) == sd.ISGOOD]
+
+    if len(args.sweep_data) > 1:
+        sweepB = sd.FreqSweep(args.sweep_data[1])
+        mdB = sd.SweepMetadata(file=args.metadata[1])
+        freqsB = mdB.freq[(mdB.flag & sd.ISGOOD) == sd.ISGOOD]
+        loA, loB = findLOs(freqsA, sweepA.lo, freqsB, sweepB.lo)
+        useB = True
+
+    else:
+        loA, _ = findLOs(freqsA, sweepA.lo)
+        useB = False
+
 
     if args.config is not None:
         cfg = mkidcore.config.load(args.config)
-        rnumA = getRoachNumberFromFile(sweepA)
-        rnumB = getRoachNumberFromFile(sweepB)
-        cfg.register('r{}.lo'.format(rnumA), loA)
-        cfg.register('r{}.lo'.format(rnumB), loB)
+        rnumA = getRoachNumberFromFile(os.path.basename(args.sweep_data[0]))
+        cfg.register('r{}.lo_freq'.format(rnumA), float(loA), update=True)
+        if useB:
+            rnumB = getRoachNumberFromFile(os.path.basename(args.sweep_data[1]))
+            cfg.register('r{}.lo_freq'.format(rnumB), float(loB), update=True)
+
+        cfg.save(args.config)
         
