f9e3203eb0c2113be8323fe800fce1b044963f1f
neelay <neelay@physics.ucsb.edu>
2020-01-28 18:30:47 -0800

    add extensive input file pattern matching to correctFreqShift

--- mkidreadout/configuration/correctFreqShift.py
+++ mkidreadout/configuration/correctFreqShift.py
@@ -12,10 +12,11 @@ Algorithm:
     2. Fit (f_iqvpeak - f_mdfile) vs f_mdfile to polynomial
     3. Use polynomial fit to correct frequencies in MD file
 """
-import os, argparse
+import os, argparse, glob, parse
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+
 import mkidreadout.configuration.sweepdata as sd
 
 def findIQVPeaks(freqs, sweep, winSize=None, scaleWin=False, convWin=11):
@@ -73,67 +74,132 @@ if __name__=='__main__':
     parser.add_argument('--snap', action='store_true', help='Final snap in small: something like (50*3.5e9/f0) kHz')
     args = parser.parse_args()
 
-    sweep = sd.FreqSweep(args.sweep)
-    metadata = sd.SweepMetadata(file=args.metadata)
+    sweepGlobPat = args.sweep.replace('{roach}', '???')
+    mdGlobPat = args.metadata.replace('{roach}', '???')
+    sweepGlobPat = sweepGlobPat.replace('{fl}', '*')
+    mdGlobPat = mdGlobPat.replace('{fl}', '*')
+    sweepGlobPat = sweepGlobPat.replace('{feedline}', '??')
+    mdGlobPat = mdGlobPat.replace('{feedline}', '??')
+    sweepGlobPat = sweepGlobPat.replace('{range}', '?')
+    mdGlobPat = mdGlobPat.replace('{range}', '?')
 
-    if args.metadata_out is None:
-        outFile = args.metadata.split('.')[0] + '_corrected.txt'
-        if args.snap:
-            outFile = outFile.split('.')[0] + '_snap.txt'
-    else:
-        outFile = args.metadata_out
+    print sweepGlobPat
+    print mdGlobPat
 
-    goodMask = (metadata.flag & sd.ISGOOD).astype(bool)
-    freqsToFix = metadata.freq[goodMask]
+    sweepFiles = np.array(glob.glob(sweepGlobPat))
+    mdFiles = np.array(glob.glob(mdGlobPat))
 
-    iqvPeaks = findIQVPeaks(freqsToFix, sweep)
-    medCorFreqs = freqsToFix + np.median(iqvPeaks - freqsToFix) #shift by median deviation
-    iqvPeaks = findIQVPeaks(medCorFreqs, sweep, winSize=1.e6) #find IQV peaks around median-centered data to remove bias
-    fittedFreqs, fitParams = fitDeltaF(freqsToFix, iqvPeaks) 
+    if len(sweepFiles) == len(mdFiles) == 1:
+        mdFilesOrdered = mdFiles
+        paramDicts = [{}]
 
-    fig = plt.figure()
-    ax0 = fig.add_subplot(211)
-    ax1 = fig.add_subplot(212)
+    else: #find md file corresponding to each sweep
+        mdFilesOrdered = []
+        paramDicts = [] #list of parsed out params for each file
+        sweepFmt = args.sweep.replace('*', '{}')
+        mdFmt = args.metadata.replace('*', '{}')
 
-    ax0.plot(freqsToFix, iqvPeaks - freqsToFix, '.')
-    ax0.plot(freqsToFix, fittedFreqs - freqsToFix, '-')
-    ax1.plot(freqsToFix, iqvPeaks - fittedFreqs, '.')
-    ax0.set_title('Initial (noisy) fit')
-    plt.show()
+        sweepFmt = sweepFmt.replace('{roach}', '{roach:d}')
+        sweepFmt = sweepFmt.replace('{fl}', '{fl:d}')
+        sweepFmt = sweepFmt.replace('{feedline}', '{feedline:d}')
+        mdFmt = mdFmt.replace('{roach}', '{roach:d}')
+        mdFmt = mdFmt.replace('{fl}', '{fl:d}')
+        mdFmt = mdFmt.replace('{feedline}', '{feedline:d}')
 
-    refinedIQVPeaks = findIQVPeaks(fittedFreqs, sweep, winSize=600.e3)
-    refinedFreqs, fitParams = fitDeltaF(freqsToFix, refinedIQVPeaks)
+        print mdFmt
+        print sweepFmt
 
-    fig = plt.figure()
-    ax0 = fig.add_subplot(211)
-    ax1 = fig.add_subplot(212)
+        for sweepFile in sweepFiles:
+            sweepParamDict = parse.parse(sweepFmt, sweepFile).named
+            mdMatchesSweep = np.ones(len(mdFiles)).astype(bool)
+            for i, mdFile in enumerate(mdFiles):
+                mdParamDict = parse.parse(mdFmt, mdFile).named
+                for mdKey, mdVal in mdParamDict.items():
+                    if sweepParamDict.has_key(mdKey):
+                        if sweepParamDict[mdKey] != mdVal:
+                            mdMatchesSweep[i] = False #make sure all overlapping keys match
 
-    ax0.plot(freqsToFix, refinedIQVPeaks - freqsToFix, '.')
-    ax0.plot(freqsToFix, refinedFreqs - freqsToFix, '-')
-    ax1.plot(freqsToFix, refinedIQVPeaks - refinedFreqs, '.')
-    ax0.set_title('Fit after 1 Refinements')
-    plt.show()
+            if np.sum(mdMatchesSweep) > 1:
+                raise Exception('Multiple metadata files matching for {}'.format(sweepFile))
+            if np.sum(mdMatchesSweep) == 0:
+                warnings.warn('No metadata found for {}. Skipping.'.format(sweepFile))
+                mdFilesOrdered.append(None)
 
-    #refinedIQVPeaks = findIQVPeaks(refinedFreqs, sweep, winSize=250.e3, convWin=0)
-    #refinedFreqs, fitParams = fitDeltaF(freqsToFix, refinedIQVPeaks)
+            matchingMD = mdFiles[mdMatchesSweep][0]
+            mdParamDict = parse.parse(mdFmt, matchingMD)
+            sweepParamDict.update(mdParamDict)
+            paramDicts.append(sweepParamDict)
+            mdFilesOrdered.append(matchingMD)
 
-    #fig = plt.figure()
-    #ax0 = fig.add_subplot(211)
-    #ax1 = fig.add_subplot(212)
+        if len(mdFilesOrdered) != len(np.unique(mdFilesOrdered)):
+            raise Exception('Duplicate MD files')
 
-    #ax0.plot(freqsToFix, refinedIQVPeaks - freqsToFix, '.')
-    #ax0.plot(freqsToFix, refinedFreqs - freqsToFix, '-')
-    #ax1.plot(freqsToFix, refinedIQVPeaks - refinedFreqs, '.')
-    #ax0.set_title('Fit after 2 Refinements')
-    #plt.show()
+    for i in range(len(sweepFiles)):
+        print '{}: {} ... {}'.format(sweepFiles[i], mdFilesOrdered[i], paramDicts[i])
 
-    if args.snap:
-        finalSnapFreqs = findIQVPeaks(refinedFreqs, sweep, winSize=50.e3, scaleWin=True, convWin=3)
-        metadata.freq[goodMask] = finalSnapFreqs
-        plt.plot(freqsToFix, finalSnapFreqs - refinedFreqs)
+    for i in range(len(sweepFiles)):
+        sweep = sd.FreqSweep(sweepFiles[i])
+        metadata = sd.SweepMetadata(file=mdFilesOrdered[i])
+
+        if args.metadata_out is None:
+            outFile = mdFilesOrdered[i].split('.')[0] + '_corrected.txt'
+            if args.snap:
+                outFile = outFile.split('.')[0] + '_snap.txt'
+        else:   
+            outFile = args.metadata_out.format(**paramDicts[i])
+
+        goodMask = (metadata.flag & sd.ISGOOD).astype(bool)
+        freqsToFix = metadata.freq[goodMask]
+
+        iqvPeaks = findIQVPeaks(freqsToFix, sweep)
+        medCorFreqs = freqsToFix + np.median(iqvPeaks - freqsToFix) #shift by median deviation
+        iqvPeaks = findIQVPeaks(medCorFreqs, sweep, winSize=1.e6) #find IQV peaks around median-centered data to remove bias
+        fittedFreqs, fitParams = fitDeltaF(freqsToFix, iqvPeaks) 
+
+        fig = plt.figure()
+        ax0 = fig.add_subplot(211)
+        ax1 = fig.add_subplot(212)
+
+        ax0.plot(freqsToFix, iqvPeaks - freqsToFix, '.')
+        ax0.plot(freqsToFix, fittedFreqs - freqsToFix, '-')
+        ax1.plot(freqsToFix, iqvPeaks - fittedFreqs, '.')
+        ax0.set_title('roach: {roach}, fl: {fl}{range}: initial (noisy) fit'.format(**paramDicts[i]))
         plt.show()
-    else:
-        metadata.freq[goodMask] = refinedFreqs
 
-    metadata.save(outFile)
+        refinedIQVPeaks = findIQVPeaks(fittedFreqs, sweep, winSize=600.e3)
+        refinedFreqs, fitParams = fitDeltaF(freqsToFix, refinedIQVPeaks)
+
+        fig = plt.figure()
+        ax0 = fig.add_subplot(211)
+        ax1 = fig.add_subplot(212)
+
+        ax0.plot(freqsToFix, refinedIQVPeaks - freqsToFix, '.')
+        ax0.plot(freqsToFix, refinedFreqs - freqsToFix, '-')
+        ax1.plot(freqsToFix, refinedIQVPeaks - refinedFreqs, '.')
+        ax0.set_title('roach: {roach}, fl: {fl}{range}: fit after 1 refinements'.format(**paramDicts[i]))
+        plt.show()
+
+        #refinedIQVPeaks = findIQVPeaks(refinedFreqs, sweep, winSize=250.e3, convWin=0)
+        #refinedFreqs, fitParams = fitDeltaF(freqsToFix, refinedIQVPeaks)
+
+        #fig = plt.figure()
+        #ax0 = fig.add_subplot(211)
+        #ax1 = fig.add_subplot(212)
+
+        #ax0.plot(freqsToFix, refinedIQVPeaks - freqsToFix, '.')
+        #ax0.plot(freqsToFix, refinedFreqs - freqsToFix, '-')
+        #ax1.plot(freqsToFix, refinedIQVPeaks - refinedFreqs, '.')
+        #ax0.set_title('Fit after 2 Refinements')
+        #plt.show()
+
+        if args.snap:
+            finalSnapFreqs = findIQVPeaks(refinedFreqs, sweep, winSize=50.e3, scaleWin=True, convWin=3)
+            metadata.freq[goodMask] = finalSnapFreqs
+            plt.plot(freqsToFix, finalSnapFreqs - refinedFreqs)
+            plt.title('roach: {roach}, fl: {fl}{range}: final snap deltaf'.format(**paramDicts[i]))
+            plt.show()
+        else:
+            metadata.freq[goodMask] = refinedFreqs
+
+        metadata.save(outFile)
     
