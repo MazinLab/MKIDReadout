8bb7582810bcab7a6b2e42b8cc22be29bf620998
abwalter <abwalter@physics.ucsb.edu>
2020-05-29 23:05:45 -1000

    old changes to sendUARTInit

--- mkidreadout/channelizer/sendUARTInit.py
+++ mkidreadout/channelizer/sendUARTInit.py
@@ -1,7 +1,14 @@
 # TODO Merge with roach2controls main
 
 import Roach2Controls as r
+import argparse
+import os, sys
 
-roach = r.Roach2Controls('10.0.0.112', 'darknessfpga.param')
-roach.connect()
-roach.sendUARTCommand(1)
+parser = argparse.ArgumentParser()
+parser.add_argument('roaches', nargs='+')
+args = parser.parse_args()
+
+for roach in args.roaches:
+    roach = r.Roach2Controls('10.0.0.' + str(roach), 'darknessfpga.param')
+    roach.connect()
+    roach.sendUARTCommand(1)
