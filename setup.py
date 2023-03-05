f65ab47bef76f1575d4c0702b311c4ea22fdd519
Jeb Bailey <baileyji@ucsb.edu>
2023-03-05 01:56:20 -0300

    add photons applet

--- setup.py
+++ setup.py
@@ -102,7 +102,8 @@ setuptools.setup(
     package_data={'mkidreadout': ('config/*.yml', 'resources/firmware/*', 'resources/firfilters/*')},
     scripts=['mkidreadout/channelizer/initgui.py',
              'mkidreadout/channelizer/hightemplar.py',
-             'mkidreadout/readout/dashboard.py',
+             'mkidreadout/readout/dashboard.py'
+             'mkidreadout/readout/send_photon_applet.py',
              'mkidreadout/channelizer/reinitADCDAC.py',
              'mkidreadout/configuration/powersweep/clickthrough_hell.py',
              'mkidreadout/configuration/powersweep/ml/findResonatorsWPS.py',
